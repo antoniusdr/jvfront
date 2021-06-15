cc832cabf02316039aad8b50832fdcbcff057205
antoniusdr <antoniusr3101@gmail.com>
2021-06-15 11:16:00 +0200

    added different pages and routes

---
+++ src/pages/HallOfFame/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HallOfFame() {
+  return <div></div>;
+}
+
+export default HallOfFame;
---
+++ src/pages/SubmitSubmission/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SubmitSubmission() {
+  return <div></div>;
+}
+
+export default SubmitSubmission;
